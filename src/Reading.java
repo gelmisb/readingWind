6ab284cb79df4806e40a9efc90051cd3e2be34fc
GelmisBartulis <gelmis.bartulis@gmail.com>
2020-02-09 13:50:39 +0000

    End of day - progress 1/2

--- src/Reading.java
+++ src/Reading.java
@@ -17,7 +17,6 @@ public class Reading {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     private static void readFile() throws IOException {
@@ -26,13 +25,11 @@ public class Reading {
         Pattern pattern = Pattern.compile(",");
 
         //Reader to read the file
-
         try (BufferedReader in = new BufferedReader(new FileReader("data_file.csv"))) {
-
             // List model for creating and populating the data
             List<WindDataModel> windData = in
                     .lines()
-                    .skip(1)
+                    .skip(0)
                     .map(line -> {
                         String[] x = pattern.split(line);
                         return new WindDataModel(x[0],
@@ -44,14 +41,17 @@ public class Reading {
                                 x[6]);
                     })
                     .collect(Collectors.toList());
+
+            // The output processes can be done with one of the latter libraries
+            // 1. Using Google Gson library
             String json = new Gson().toJson(windData);
             System.out.println(json);
 
+            // 2. Using ObjectMapper from Jackson
             ObjectMapper objectMapper = new ObjectMapper();
             String jsonString = objectMapper.writeValueAsString(windData);
             System.out.println(jsonString);
-
-
+            // Either options work fast and do not bring a huge load to the processing
         } catch (IOException e) {
             e.printStackTrace();
         }
